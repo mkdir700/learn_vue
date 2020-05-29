387ac07a26b7d1bf0063c81ee3b15e125bcbe089
mkdir700 <mkdir700@gmail.com>
2020-05-29 17:03:38 +0800

    请求获取用户列表api

--- src/components/users/UserList.vue
+++ src/components/users/UserList.vue
@@ -14,7 +14,7 @@
                 <div style="margin-top: 15px;">
                     <el-row :gutter="20">
                         <el-col :span="7">
-                            <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+                            <el-input placeholder="请输入内容" class="input-with-select">
                                 <el-button slot="append" icon="el-icon-search"></el-button>
                             </el-input>
                         </el-col>
@@ -30,7 +30,26 @@
 
 <script>
     export default {
-        name: "UserList"
+        name: "UserList",
+        data() {
+            return {
+                queryParams: {query: '', pagenum: '1', pagesize: '2'},
+                userList: [],
+                total: 0
+            }
+        },
+        created() {
+            this.getUserList()
+        },
+        methods: {
+            async getUserList() {
+                const {data: res} = await this.$http.get('users', {params: this.queryParams})
+                if (res.meta.status !== 200) return this.$message.error('获取失败')
+                this.userList = res.data.users
+                this.total = res.data.total
+                console.log(this.userList)
+            }
+        }
     }
 </script>
 
