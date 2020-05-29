38ec42ef00f2bc354dc34da1a74195e6bd8d6395
mkdir700 <mkdir700@gmail.com>
2020-05-29 09:52:42 +0800

    退出功能

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,10 +1,21 @@
 <template>
-    <p>Home</p>
+    <div class="xxx">
+        <p>Home</p>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "Home"
+        name: "Home",
+        methods: {
+            logout() {
+                /*清空*/
+                window.sessionStorage.clear()
+                /*跳转到登录页面*/
+                this.$router.push({name: 'login'})
+            }
+        }
     }
 </script>
 
