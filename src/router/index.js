78ddba27697e1ca467bd5a6b8d859ece1c5c355f
mkdir700 <mkdir700@gmail.com>
2020-05-29 09:24:20 +0800

    路由导航守卫 https://router.vuejs.org/zh/guide/advanced/navigation-guards.html#%E5%AF%BC%E8%88%AA%E5%AE%88%E5%8D%AB

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,27 @@ import Home from "@/components/home/Home";
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     routes: [
         {path: '/', redirect: '/login'},
         {path: '/login', name: 'login', component: Login},
         {path: '/home', name: 'home', component: Home}
     ]
 })
+
+/*to 将要访问的路径
+* from 从两个路径跳转而来
+* next是一个函数，表示放行
+* 直接next表示放行，next()表示强制跳转到括号内的路径*/
+router.beforeEach((to, from, next)=>{
+    /*通过路径来判断*/
+    // if (to.path === '/login') return next()
+    /*通过命名来判断*/
+    if (to.name === 'login') return next()
+    /*获取token*/
+    const tokenStr = window.sessionStorage.getItem("token")
+    if (!tokenStr) return next({name: 'login'})
+    return next()
+})
+
+export default router
