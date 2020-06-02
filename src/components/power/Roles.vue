1f516d37fbbdbde61c572c61c77261523b6eda3f
mkdir700 <mkdir700@gmail.com>
2020-06-02 14:37:23 +0800

    功能: 权限展开的列表绘制

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -23,11 +23,13 @@
                         <!--二级权限-->
                         <el-col :span="19">
                             <el-row :class="['bdtop', {'bdtop': i2!=0}]" v-for="(item2, i2) in item.children" :key="item2.id">
-                                <el-col>
+                                <el-col :span="6">
                                     <el-tag type="success">{{item2.authName}}</el-tag>
                                     <i class="el-icon-arrow-right"></i>
                                 </el-col>
-                                <el-col></el-col>
+                                <el-col :span="13">
+                                    <el-tag type="warning" v-for="(item3, i3) in item2.children" :key="item3.id">{{item.authName}}</el-tag>
+                                </el-col>
                             </el-row>
                         </el-col>
                     </el-row>
